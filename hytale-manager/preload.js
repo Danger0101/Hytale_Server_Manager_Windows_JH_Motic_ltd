ef755fa950a6b2bbb60371052ef86fc38a6e7613
John Hummel <34178990+Danger0101@users.noreply.github.com>
2026-01-18 16:34:35 +0000

    Update preload.js

--- hytale-manager/preload.js
+++ hytale-manager/preload.js
@@ -1,47 +1,59 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  // --- Server Management ---
-  getServers: () => ipcRenderer.invoke('get-servers'),
-  addServer: (serverData) => ipcRenderer.invoke('add-server', serverData),
-  updateServer: (serverData) => ipcRenderer.invoke('update-server', serverData),
-  deleteServer: (data) => ipcRenderer.invoke('delete-server', data),
-  selectDirectory: () => ipcRenderer.invoke('select-directory'),
-  openFolder: (path) => ipcRenderer.send('open-folder', path),
-  openBackupFolder: (serverId) => ipcRenderer.invoke('open-backup-folder', serverId),
-  backupServer: (serverId) => ipcRenderer.invoke('backup-server', serverId),
-  installServerJar: (serverId) => ipcRenderer.invoke('install-server-jar', serverId),
-  checkJarExists: (serverId) => ipcRenderer.invoke('check-jar-exists', serverId),
-  importFromLauncher: (serverId) => ipcRenderer.invoke('import-from-launcher', serverId),
-  lookupHytalePlayer: (data) => ipcRenderer.invoke('lookup-hytale-player', data),
-  reportPlayer: (serverId, playerId, reason) => ipcRenderer.invoke('report-hytale-player', { serverId, playerId, reason }),
-  checkHytaleVersion: (serverId) => ipcRenderer.invoke('check-hytale-version', serverId),
-  installViaCli: (serverId) => ipcRenderer.invoke('install-via-cli', serverId),
+    // --- Server Management ---
+    getServers: () => ipcRenderer.invoke('get-servers'),
+    addServer: (server) => ipcRenderer.invoke('add-server', server),
+    updateServer: (server) => ipcRenderer.invoke('update-server', server),
+    deleteServer: (data) => ipcRenderer.invoke('delete-server', data),
+    
+    // NEW: Auth Functions
+    authStartDeviceFlow: () => ipcRenderer.invoke('auth-start-device-flow'),
+    authPollToken: (code) => ipcRenderer.invoke('auth-poll-token', code),
+    
+    // --- File System ---
+    selectDirectory: () => ipcRenderer.invoke('select-directory'),
+    selectFile: (filter) => ipcRenderer.invoke('select-file', filter),
+    openFolder: (path) => ipcRenderer.send('open-folder', path),
+    openBackupFolder: (id) => ipcRenderer.invoke('open-backup-folder', id),
+    backupServer: (id) => ipcRenderer.invoke('backup-server', id),
+    
+    // --- Config & files ---
+    readFile: (data) => ipcRenderer.invoke('read-file', data),
+    saveFile: (data) => ipcRenderer.invoke('save-file', data),
 
-  // --- TOOL AUTO-SETUP (NEW) ---
-  checkCliTool: () => ipcRenderer.invoke('check-cli-tool'),
-  downloadCliTool: () => ipcRenderer.invoke('download-cli-tool'),
-  selectFile: (filter) => ipcRenderer.invoke('select-file', filter), // New file browser
-  checkJavaInstalled: () => ipcRenderer.invoke('check-java-installed'),
-  downloadJava: () => ipcRenderer.invoke('download-java'),
-  
-  // --- NETWORK & PERFORMANCE AUTOMATION ---
-  setupFirewall: (port) => ipcRenderer.invoke('setup-firewall', port),
-  getPublicIp: () => ipcRenderer.invoke('get-public-ip'),
-  checkAotFile: (serverId) => ipcRenderer.invoke('check-aot-file', serverId),
-  
+    // --- Server Control ---
+    startServer: (id) => ipcRenderer.send('start-server', id),
+    stopServer: (id) => ipcRenderer.send('stop-server', id),
+    sendCommand: (data) => ipcRenderer.send('send-command', data),
+    
+    // --- Installation & Tools ---
+    installServerJar: (id) => ipcRenderer.invoke('install-server-jar', id),
+    checkJarExists: (id) => ipcRenderer.invoke('check-jar-exists', id),
+    importFromLauncher: (id) => ipcRenderer.invoke('import-from-launcher', id),
+    
+    // Hytale Downloader CLI
+    checkCliTool: () => ipcRenderer.invoke('check-cli-tool'),
+    downloadCliTool: () => ipcRenderer.invoke('download-cli-tool'),
+    installViaCli: (id) => ipcRenderer.invoke('install-via-cli', id),
+    
+    // Java Management
+    checkJavaInstalled: () => ipcRenderer.invoke('check-java-installed'),
+    downloadJava: () => ipcRenderer.invoke('download-java'),
 
-  // File Editing
-  readFile: (data) => ipcRenderer.invoke('read-file', data),
-  saveFile: (data) => ipcRenderer.invoke('save-file', data),
+    // --- System & Network ---
+    getPublicIp: () => ipcRenderer.invoke('get-public-ip'),
+    setupFirewall: (port) => ipcRenderer.invoke('setup-firewall', port),
+    checkAotFile: (id) => ipcRenderer.invoke('check-aot-file', id),
+    checkHytaleVersion: (id) => ipcRenderer.invoke('check-hytale-version', id),
 
+    // --- Hytale Web API ---
+    lookupHytalePlayer: (data) => ipcRenderer.invoke('lookup-hytale-player', data),
+    reportPlayer: (id, uuid, reason) => ipcRenderer.invoke('report-hytale-player', {serverId: id, playerId: uuid, reason}),
 
-  // --- Server Interaction ---
-  startServer: (serverId) => ipcRenderer.send('start-server', serverId),
-  stopServer: (serverId) => ipcRenderer.send('stop-server', serverId),
-  sendCommand: (data) => ipcRenderer.send('send-command', data),
-
-  // --- Listeners from Main ---
-  onServerLog: (callback) => ipcRenderer.on('server-log', (_event, value) => callback(value)),
-  onServerStateChange: (callback) => ipcRenderer.on('server-state-change', (_event, value) => callback(value)),
-  onToolDownloadStatus: (callback) => ipcRenderer.on('tool-download-status', (_event, value) => callback(value)),
+    // --- Listeners (Main -> Renderer) ---
+    onServerLog: (callback) => ipcRenderer.on('server-log', (_event, value) => callback(value)),
+    onServerStateChange: (callback) => ipcRenderer.on('server-state-change', (_event, value) => callback(value)),
+    onAuthNeeded: (callback) => ipcRenderer.on('auth-needed', (_event, value) => callback(value)),
+    onToolDownloadStatus: (callback) => ipcRenderer.on('tool-download-status', (_event, value) => callback(value))
+});
