8e4ebad39fe58c4a8ac6c41decf066b44b7c09b3
John Hummel <34178990+Danger0101@users.noreply.github.com>
2026-01-18 13:17:55 +0000

    Update preload.js

--- hytale-manager/preload.js
+++ hytale-manager/preload.js
@@ -18,6 +18,19 @@ contextBridge.exposeInMainWorld('electronAPI', {
   checkHytaleVersion: (serverId) => ipcRenderer.invoke('check-hytale-version', serverId),
   installViaCli: (serverId) => ipcRenderer.invoke('install-via-cli', serverId),
 
+  // --- TOOL AUTO-SETUP (NEW) ---
+  checkCliTool: () => ipcRenderer.invoke('check-cli-tool'),
+  downloadCliTool: () => ipcRenderer.invoke('download-cli-tool'),
+  selectFile: (filter) => ipcRenderer.invoke('select-file', filter), // New file browser
+  checkJavaInstalled: () => ipcRenderer.invoke('check-java-installed'),
+  downloadJava: () => ipcRenderer.invoke('download-java'),
+  
+  // --- NETWORK & PERFORMANCE AUTOMATION ---
+  setupFirewall: (port) => ipcRenderer.invoke('setup-firewall', port),
+  getPublicIp: () => ipcRenderer.invoke('get-public-ip'),
+  checkAotFile: (serverId) => ipcRenderer.invoke('check-aot-file', serverId),
+  
+
   // File Editing
   readFile: (data) => ipcRenderer.invoke('read-file', data),
   saveFile: (data) => ipcRenderer.invoke('save-file', data),
@@ -31,4 +44,4 @@ contextBridge.exposeInMainWorld('electronAPI', {
   // --- Listeners from Main ---
   onServerLog: (callback) => ipcRenderer.on('server-log', (_event, value) => callback(value)),
   onServerStateChange: (callback) => ipcRenderer.on('server-state-change', (_event, value) => callback(value)),
-});
+  onToolDownloadStatus: (callback) => ipcRenderer.on('tool-download-status', (_event, value) => callback(value)),
